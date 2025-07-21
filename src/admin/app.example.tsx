b87d77ee28865354b3c24549a6197cb3b1bd5b68
fahadpj23 <fahadpj23@gmail.com>
2025-07-21 12:25:56 +0530

    tr

--- src/admin/app.example.tsx
+++ src/admin/app.example.tsx
@@ -1,4 +1,4 @@
-import type { StrapiApp } from '@strapi/strapi/admin';
+import type { StrapiApp } from "@strapi/strapi/admin";
 
 export default {
   config: {
@@ -31,6 +31,7 @@ export default {
       // 'zh',
     ],
   },
+
   bootstrap(app: StrapiApp) {
     console.log(app);
   },
